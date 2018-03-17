4a78af810f9fd79ff02cec72113fc2a095c2e8f0
nametable <lbateman0199@gmail.com>
2018-03-17 16:08:40 -0400

    New BackgroundService Class

--- backgroundservice.cpp
+++ backgroundservice.cpp
@@ -1,6 +1,31 @@
 #include "backgroundservice.h"
 
-BackgroundService::BackgroundService()
+BackgroundService::BackgroundService(int argc, char *argv[], QObject *parent) : QObject(parent)
+{
+    this->runningconfig=new ScoreCheckingConfig; //create an instance of ScoreCheckingConfig
+    this->app= new QCoreApplication(argc,argv);
+}
+void BackgroundService::LoadConfig(std::string configpath)
+{
+    loadConfigBIN(*this->runningconfig, configpath.c_str());
+}
+void BackgroundService::Run()
 {
 
+    std::cout << "ScoringEngine background mode started...\n";
+    this->setup();
+    this->app->exec();
+}
+void BackgroundService::mainloop()
+{
+    //std::cout << "More Done??...\n";
+    this->curtime=std::chrono::system_clock::now();
+    this->runningconfig->GenerateScoreReport();
+}
+void BackgroundService::setup()
+{
+    this->timer=new QTimer();
+    connect(timer, SIGNAL(timeout()),this, SLOT(mainloop()));
+    if (this->runningconfig->checkSeconds<1)this->runningconfig->checkSeconds=30;
+    timer->start(this->runningconfig->checkSeconds*1000);
 }
--- backgroundservice.h
+++ backgroundservice.h
@@ -1,11 +1,28 @@
 #ifndef BACKGROUNDSERVICE_H
 #define BACKGROUNDSERVICE_H
 
-
-class BackgroundService
+#include <string>
+#include <chrono>
+#include <ctime>
+#include <QTimer>
+#include <QApplication>
+#include "scorecheckingconfig.h"
+class BackgroundService : public QObject
 {
+    Q_OBJECT
 public:
-    BackgroundService();
+    void LoadConfig(std::string configpath);
+    void Run();
+    explicit BackgroundService(int argc, char *argv[], QObject *parent = nullptr);
+
+private:
+    void setup();
+    ScoreCheckingConfig *runningconfig;
+    QCoreApplication *app;
+    QTimer *timer;
+    std::chrono::system_clock::time_point curtime;
+private slots:
+    void mainloop();
 };
 
 #endif // BACKGROUNDSERVICE_H
