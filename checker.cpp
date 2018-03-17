4600f9d7713a0f8f50d26abaa736b0ac1d5b836a
nametable <lbateman0199@gmail.com>
2018-03-17 16:08:22 -0400

    Remove unneeded files

--- checker.cpp
+++
@@ -1,8 +0,0 @@
-#include "checker.h"
-#include "pathexistscorechecker.h"
-#include "runcommandscorechecker.h"
-//void addCheckers()
-//{
-//    checker.push_back(new PathExistScoreChecker);
-//    checker.push_back(new RunCommandScoreChecker);
-//}
