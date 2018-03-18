6b3c704a1f26a132e5aca999ef5caaaa059c3fa1
nametable7 <lbateman0199@gmail.com>
2018-03-18 14:41:01 -0400

    Catch Unix Signals and remove QtMultimedia dependency

--- main.cpp
+++ main.cpp
@@ -2,16 +2,17 @@
 #include "backgroundservice.h"
 #include <iostream>
 #include <string>
-void tick()
-{
-
-}
+#include <cstdlib>
+#include <unistd.h>
+#include <signal.h>
+void catchUnixSignals(std::initializer_list<int> quitSignals);
 int main(int argc, char *argv[])
 {
     if (argc>=2) //two or more arguments
     {
         if (std::string(argv[1])=="--background")
         {
+            catchUnixSignals({SIGQUIT, SIGINT, SIGTERM, SIGHUP});
             BackgroundService bkserv(argc,argv);
             bkserv.LoadConfig("config.bin");
             bkserv.Run();
@@ -24,4 +25,24 @@ int main(int argc, char *argv[])
     w.show();
 
     return a.exec();
+}
+void catchUnixSignals(std::initializer_list<int> quitSignals) {
+    auto handler = [](int sig) -> void {
+        // blocking and not aysnc-signal-safe func are valid
+        printf("\nquit the application by signal(%d).\n", sig);
+        QCoreApplication::quit();
+    };
+
+    sigset_t blocking_mask;
+    sigemptyset(&blocking_mask);
+    for (auto sig : quitSignals)
+        sigaddset(&blocking_mask, sig);
+
+    struct sigaction sa;
+    sa.sa_handler = handler;
+    sa.sa_mask    = blocking_mask;
+    sa.sa_flags   = 0;
+
+    for (auto sig : quitSignals)
+        sigaction(sig, &sa, nullptr);
 }
