ffd3d469d329477407c58684197a0ff7536d9e01
Logan Bateman <lbateman0199@gmail.com>
2018-08-29 14:52:04 -0400

    Add text edit - testing

--- playerdashboard.cpp
+++ playerdashboard.cpp
@@ -1,13 +1,16 @@
 #include "playerdashboard.h"
 #include "ui_playerdashboard.h"
 #include "qradiobutton.h"
+#include "qplaintextedit.h"
 PlayerDashboard::PlayerDashboard(QWidget *parent, ScoreCheckingConfig *config) :
     QMainWindow(parent),
     ui(new Ui::PlayerDashboard)
 {
     ui->setupUi(this);
     for (unsigned int i=0; i<10; i++){
-        this->centralWidget()->layout()->addWidget(new QRadioButton(QString("Test Radio ")+ QString(i),this));
+        this->centralWidget()->layout()->addWidget(new QRadioButton(QString("Test Radio ")+ QString(std::to_string(i).c_str()),this));
+        this->centralWidget()->layout()->addWidget(new QPlainTextEdit(QString("Test Plain Text Edit ")+ QString(std::to_string(i).c_str()),this));
+
     }
 
 }
