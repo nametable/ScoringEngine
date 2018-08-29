2fb5711d933fe424378ee60a4b4446c01dfb9963
Logan Bateman <lbateman0199@gmail.com>
2018-08-29 18:52:03 -0400

    Initial constants.h

---
+++ constants.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+//constants
+const unsigned int COLUMN_TYPE=0;
+const unsigned int COLUMN_DESCRIPTION=1;
+const unsigned int COLUMN_INSTRUCTIONS=2; //very new - testing
+const unsigned int COLUMN_OPT1=3;
+const unsigned int COLUMN_OPT2=4;
+const unsigned int COLUMN_SEARCH_STR=5;
+const unsigned int COLUMN_POINTS=6;
+const unsigned int COLUMN_FIXED=7;
+
+const unsigned int CHECKER_PATH=0;
+const unsigned int CHECKER_RUN_COMMAND=1;
+const unsigned int CHECKER_VALUE=2;
+const unsigned int CHECKER_RUN_SCRIPT=3;
+const unsigned int CHECKER_QA=4;
+const unsigned int CHECKER_TEMPLATE=5;
+#endif // CONSTANTS_H
