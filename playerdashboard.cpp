51a82305de284d9ecc8a4f4be28acdbf9a7d6be4
Logan Bateman <lbateman0199@gmail.com>
2018-08-29 14:30:55 -0400

    Initial player dashboard test

--- playerdashboard.cpp
+++ playerdashboard.cpp
@@ -1,12 +1,12 @@
 #include "playerdashboard.h"
 #include "ui_playerdashboard.h"
-
+#include "qradiobutton.h"
 PlayerDashboard::PlayerDashboard(QWidget *parent, ScoreCheckingConfig *config) :
     QMainWindow(parent),
     ui(new Ui::PlayerDashboard)
 {
     ui->setupUi(this);
-    //this->add
+    this->centralWidget()->layout()->addWidget(new QRadioButton("Test Radio",this));
 }
 
 PlayerDashboard::~PlayerDashboard()
